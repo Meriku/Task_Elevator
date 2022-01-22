c9f775378006c0d35a11ec2d3d82a14fc54e253e
Professional <Professional@USER-PC>
2022-01-22 18:10:09 +0200

    Добавьте файлы проекта.

---
+++ Task_Elevator/Elevator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Elevator
+{
+    internal class Elevator
+    {
+        public static string GetResult (int PersonsCount, int AvarageWeight)
+        {
+            var rnd = new Random();
+            var LoadCapacity = rnd.Next(100, 900);                                      // грузоподъемность 
+            double LoadCapacityReserv = LoadCapacity - (PersonsCount * AvarageWeight);  // резерв грузоподъмности, или соттвественно перегруз
+            double PersonsReserv = LoadCapacityReserv / AvarageWeight;                  // резерв грузоподъемности в людях 
+
+            if (LoadCapacityReserv >= AvarageWeight) //если лифт вмещает этих людей и остается место как минимум для одного человека
+            {       
+                return $"\nГрузоподъемность лифта = {LoadCapacity} кг.\nЛифт сможет перевезти {PersonsCount} человек.\n" +
+                    $"Места хватит еще на {Math.Truncate(PersonsReserv)} человек."; //округляем число отбрасывая значение после точки  
+
+            }
+            if (LoadCapacity >= 0 && LoadCapacity < AvarageWeight) // если грузоподъемности хватает но больше человек не влезет
+            {
+                return $"\nГрузоподъемность лифта = {LoadCapacity} кг.\nЛифт сможет перевезти максимум {PersonsCount} человека.";
+            }
+            else   // если грузоподъемности не хватает
+            {
+                return $"\nГрузоподъемность лифта = {LoadCapacity} кг.\nЛифт не сможет перевезти {PersonsCount} человек.\n" +
+                    $"{-Math.Floor(PersonsReserv)} человек должны покинуть лифт для начала движения."; 
+                //PersonsReserv округляем до наименьшего целого числа, поскольку тут PersonsReserv - число отрицательное
+            }
+        }
+    }
+}
---
+++ Task_Elevator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Задание лифт
+//в лифт заходит N человек (число N вводится с клавиатуры пользователем) 
+//у лифта есть грузоподъемность - она должна быть рандомной от 100 до 500 кг. 
+//средний вес человека указывается пользователем с клавиатуры
+//выводится значение:
+//если все помещаются, то пишет " все поместились" 
+//если не поместились то писало " выкиньте одного"
+
+
+
+
+
+
+
+
+
+
+namespace Task_Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int PersonsCount;
+            int AvarageWeight;
+
+            Console.WriteLine("Введите количество человек:");
+            PersonsCount = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nВведите средний вес человека:");
+            AvarageWeight = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Elevator.GetResult(PersonsCount, AvarageWeight)); // выводим результат
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+    }
+}
